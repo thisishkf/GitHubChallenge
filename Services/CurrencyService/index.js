f4755d103e40eabfad1e0f1b1554b251f67c87ad
thisishkf <thisishkf@HOs-MacBook-Pro.local>
2018-05-13 21:56:35 +0800

    currency service fix

--- Services/CurrencyService/index.js
+++ Services/CurrencyService/index.js
@@ -3,19 +3,23 @@ const openexchangeratesService = require('./openexchangeratesService');
 const currencyList = require('./currencies.json');
 const Logger = require('../../lib/Logger');
 
-var service;
+var service = openexchangeratesService;
 
 const _getService = function () {
-    //TODO : factory module to select service
     Logger.debug("CurrencyService.getService() returning openexchangeratesService");
-    return openexchangeratesService;
+    return service;
 }
 
-const _getCurrencyList = function(){
+const _setService = function (switchService) {
+    //TODO : change Service when curreny service is down
+    service = switchService;
+}
+
+const _getCurrencyList = function () {
     return currencyList;
 }
 
 module.exports = {
-    getService : _getService,
-    getCurrencyList : _getCurrencyList
+    getService: _getService,
+    getCurrencyList: _getCurrencyList
 }
